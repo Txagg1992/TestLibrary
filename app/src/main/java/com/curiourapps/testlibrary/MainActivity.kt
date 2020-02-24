28dee1a8717babfe069606c3083043d99b1e4fee
txagg1992 <curiousaps@gmail.com>
2020-02-24 10:02:17 -0600

    Migrate BioStart to BuilderPattern.

--- app/src/main/java/com/curiourapps/testlibrary/MainActivity.kt
+++ app/src/main/java/com/curiourapps/testlibrary/MainActivity.kt
@@ -2,22 +2,20 @@ package com.curiourapps.testlibrary
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import com.curiourapps.biolibrary.BioPromptInfo
 import com.curiourapps.biolibrary.BioStart
 
 class MainActivity : FragmentActivity() {
 
-    val info = BioStart.customPromptInfo()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        BioStart.bioMet(this)
-        info
-            .setTitle("This is a Title")
-            .setDescription("And a Description")
-            .setNegativeButtonText("P/W or Cancel")
+        BioStart.BioStartBuilder(this)
+            .setTitle("Biometric Title")
+            .setDescription("Description of Biometric")
+            .setnegativeButtonText("Do Nothing")
             .build()
+            .bioMet()
     }
 }
--- biolibrary/src/main/java/com/curiourapps/biolibrary/BioStart.kt
+++ biolibrary/src/main/java/com/curiourapps/biolibrary/BioStart.kt
@@ -1,6 +1,5 @@
 package com.curiourapps.biolibrary
 
-import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import androidx.biometric.BiometricManager
@@ -8,196 +7,143 @@ import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 
-class BioStart{
+class BioStart private constructor(bioStartBuilder: BioStartBuilder) {
+
+    private val context: FragmentActivity
+    private val title: String
+    private val description: String
+    private val negativeButtonText: String
+    private var biometricPrompt: BiometricPrompt? = null
 
     companion object {
-        //private val cContext = AppliContext.getContext()
         private val TAG: String = "BioStart"
         private val TAGBIO: String = "BioOpened"
-        private var biometricPrompt: BiometricPrompt? = null
-
-        fun bioMet(context: FragmentActivity) {
-            val executor = ContextCompat.getMainExecutor(context)
-            val callback = object : BiometricPrompt.AuthenticationCallback() {
-                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                    super.onAuthenticationError(errorCode, errString)
-                    Log.d(TAG, "$errorCode :: $errString")
-                    if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON)
-                        Toast.makeText(context,
-                            "User Cancelled : $errString", Toast.LENGTH_LONG).show()
-                    if (errorCode == BiometricPrompt.ERROR_USER_CANCELED) {
-                        Toast.makeText(context,
-                            "User Cancelled : $errString", Toast.LENGTH_LONG).show()
-                    }
-                }
-
-                override fun onAuthenticationFailed() {
-                    super.onAuthenticationFailed()
-                    Log.d(TAG, "Somehow Failed")
-                }
-
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                    super.onAuthenticationSucceeded(result)
-                    Log.d(TAG, "<<< Login Success >>>")
-                }
-            }
-            biometricPrompt = BiometricPrompt(context, executor, callback)
-            openBio(context)
-            //return BiometricPrompt(fragmentActivity, executor, callback)
-        }
-
-        fun customPromptInfo(): BioPromptInfo {
-            return BioPromptInfo.Builder()
-                .setTitle("")
-                .setDescription("")
-                .setNegativeButtonText("")
-                .build()
-
-        }
-        fun createPromptInfo(): BiometricPrompt.PromptInfo {
-            return BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Log In")
-                .setDescription("Secure Biometric Login")
-                .setNegativeButtonText("Use Password")
-                .build()
-        }
-
-        private fun openBio(context: Context) {
-            Log.d(TAGBIO, "<++ Biometric opened ++>")
-            val promptInfo = customPromptInfo()
-            if (BiometricManager.BIOMETRIC_SUCCESS == BiometricManager
-                    .from(context)
-                    .canAuthenticate()
-            ) {
-                Log.d(TAGBIO, "No error detected. Login in progress.")
-                biometricPrompt?.authenticate(promptInfo)
-            } else {
-
-            }
-            if (BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE == BiometricManager
-                    .from(context)
-                    .canAuthenticate()) {
-                Log.d(TAGBIO, "This device does not have a biometric scanner.")
-                Toast.makeText(context,
-                    "This device does not have a biometric scanner.", Toast.LENGTH_LONG).show()
-            }
-            if (BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED == BiometricManager
-                    .from(context)
-                    .canAuthenticate()
-            ) {
-                Log.d(TAGBIO, "The user does not have any biometrics enrolled.")
-                Toast.makeText(
-                    context,
-                    "The user does not have any biometrics enrolled.", Toast.LENGTH_LONG
-                ).show()
-
-                //TODO take user to the biometric settings: likely show a
-                // dialog and give user a choice to set up or continue where they are.
-            }
-            if (BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE == BiometricManager
-                    .from(context)
-                    .canAuthenticate()
-            ) {
-                Log.d(TAGBIO, "Biometric hardware is unavailable.")
-                Toast.makeText(
-                    context,
-                    "Biometric hardware is unavailable. \n" +
-                            " Please wait for scanner to come online", Toast.LENGTH_LONG
-                ).show()
-            }
-
-        }
-
-
     }
 
-//    fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        openBio()
-//        biometricPrompt = bioMet()
-//    }
 
-//    fun bioMet(): BiometricPrompt {
-//
-//        val fragmentActivity = FragmentActivity()
-//        val executor = ContextCompat.getMainExecutor(context)
-//        val callback = object : BiometricPrompt.AuthenticationCallback() {
-//            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-//                super.onAuthenticationError(errorCode, errString)
-//                Log.d(TAG, "$errorCode :: $errString")
-//                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON)
-//                    Toast.makeText(context,
-//                        "User Cancelled : $errString", Toast.LENGTH_LONG).show()
-//                //finish()
-//                if (errorCode == BiometricPrompt.ERROR_USER_CANCELED) {
-//                    Toast.makeText(context,
-//                        "User Cancelled : $errString", Toast.LENGTH_LONG).show()
-//                }
-//            }
-//
-//            override fun onAuthenticationFailed() {
-//                super.onAuthenticationFailed()
-//                Log.d(TAG, "Somehow Failed")
-//            }
-//
-//            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-//                super.onAuthenticationSucceeded(result)
-//                Log.d(TAG, "<<< Login Success >>>")
-//            }
-//        }
-//        return BiometricPrompt(fragmentActivity, executor, callback)
-//        //return BiometricPrompt(FragmentActivity: context, executor, callback)
-//    }
-//
-//    private fun createPromptInfo(): BiometricPrompt.PromptInfo {
-//        return BiometricPrompt.PromptInfo.Builder()
-//            .setTitle("Log into myFios")
-//            .setDescription("Touch Fingerprint sensor")
-//            .setNegativeButtonText("Use Password")
-//            .build()
-//    }
+    fun bioMet() {
+        val executor = ContextCompat.getMainExecutor(context)
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Log.d(TAG, "$errorCode :: $errString")
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON)
+                    Toast.makeText(
+                        context,
+                        "User Cancelled : $errString", Toast.LENGTH_LONG
+                    ).show()
+                if (errorCode == BiometricPrompt.ERROR_USER_CANCELED) {
+                    Toast.makeText(
+                        context,
+                        "User Cancelled : $errString", Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
 
-//    private fun openBio(view: View) {
-//        Log.d(TAGBIO, "<++ Biometric opened ++>")
-//        val promptInfo = createPromptInfo()
-//        if (BiometricManager.BIOMETRIC_SUCCESS == BiometricManager
-//                .from(context)
-//                .canAuthenticate()
-//        ) {
-//            Log.d(TAGBIO, "No error detected. Login in progress.")
-//            biometricPrompt.authenticate(promptInfo)
-//        } else {
-//
-//        }
-//        if (BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE == BiometricManager
-//                .from(context)
-//                .canAuthenticate()
-//        ) {
-//            Log.d(TAGBIO, "This device does not have a biometric scanner.")
-//            Toast.makeText(context,
-//                "This device does not have a biometric scanner.", Toast.LENGTH_LONG).show()
-//        }
-//        if (BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED == BiometricManager
-//                .from(context)
-//                .canAuthenticate()
-//        ) {
-//            Log.d(TAGBIO, "The user does not have any biometrics enrolled.")
-//            Toast.makeText(context,
-//                "The user does not have any biometrics enrolled.", Toast.LENGTH_LONG).show()
-//
-//            //TODO take user to the biometric settings: likely show a
-//            // dialog and give user a choice to set up or continue where they are.
-//        }
-//        if (BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE == BiometricManager
-//                .from(context)
-//                .canAuthenticate()
-//        ) {
-//            Log.d(TAGBIO, "Biometric hardware is unavailable.")
-//            Toast.makeText(context,
-//                "Biometric hardware is unavailable. \n" +
-//                        " Please wait for scanner to come online", Toast.LENGTH_LONG).show()
-//        }
-//
-//    }
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Log.d(TAG, "Somehow Failed")
+            }
 
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Log.d(TAG, "<<< Login Success >>>")
+            }
+        }
+        biometricPrompt = BiometricPrompt(context, executor, callback)
+        openBio()
+    }
+
+    private fun createPromptInfo(): BiometricPrompt.PromptInfo {
+        return BiometricPrompt.PromptInfo.Builder()
+            .setTitle(title)
+            .setDescription(description)
+            .setNegativeButtonText(negativeButtonText)
+            .build()
+    }
+
+    private fun openBio() {
+        Log.d(TAGBIO, "<++ Biometric opened ++>")
+        val promptInfo = createPromptInfo()
+        if (BiometricManager.BIOMETRIC_SUCCESS == BiometricManager
+                .from(context)
+                .canAuthenticate()
+        ) {
+            Log.d(TAGBIO, "No error detected. Login in progress.")
+            biometricPrompt?.authenticate(promptInfo)
+        } else {
+
+        }
+        if (BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE == BiometricManager
+                .from(context)
+                .canAuthenticate()
+        ) {
+            Log.d(TAGBIO, "This device does not have a biometric scanner.")
+            Toast.makeText(
+                context,
+                "This device does not have a biometric scanner.", Toast.LENGTH_LONG
+            ).show()
+        }
+        if (BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED == BiometricManager
+                .from(context)
+                .canAuthenticate()
+        ) {
+            Log.d(TAGBIO, "The user does not have any biometrics enrolled.")
+            Toast.makeText(
+                context,
+                "The user does not have any biometrics enrolled.", Toast.LENGTH_LONG
+            ).show()
+
+            //TODO take user to the biometric settings: likely show a
+            // dialog and give user a choice to set up or continue where they are.
+        }
+        if (BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE == BiometricManager
+                .from(context)
+                .canAuthenticate()
+        ) {
+            Log.d(TAGBIO, "Biometric hardware is unavailable.")
+            Toast.makeText(
+                context,
+                "Biometric hardware is unavailable. \n" +
+                        " Please wait for scanner to come online", Toast.LENGTH_LONG
+            ).show()
+        }
+
+    }
+    class BioStartBuilder(val context: FragmentActivity) {
+        var title: String
+        var description: String
+        var negativeButtonText: String
+
+        init {
+            title = "Default Title"
+            description = "Secure Biometric Login"
+            negativeButtonText = "Use Password/Cancel"
+        }
+
+        fun setTitle(title: String): BioStartBuilder {
+            this.title = title
+            return this
+        }
+
+        fun setDescription(description: String): BioStartBuilder {
+            this.description = description
+            return this
+        }
+
+        fun setnegativeButtonText(negativeButtonText: String): BioStartBuilder {
+            this.negativeButtonText = negativeButtonText
+            return this
+        }
+
+        fun build(): BioStart {
+            return BioStart(this)
+        }
+    }
+    init {
+        context = bioStartBuilder.context
+        title = bioStartBuilder.title
+        description = bioStartBuilder.description
+        negativeButtonText = bioStartBuilder.negativeButtonText
+    }
 }
